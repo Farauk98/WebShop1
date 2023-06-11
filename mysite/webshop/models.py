bb9bc14c44759b49e93e69ca72aae0b9deb2caf1
Farauk98 <kornat.rafal@gmail.com>
2023-06-11 20:53:37 +0200

    add simply models Artists, Painting; testing API

---
+++ mysite/webshop/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Artists(models.Model):
+    title = models.CharField(max_length=200)
+    birth_date = models.DateField()
+    death_date = models.DateField()
+   
+class Painting(models.Model):
+    title = models.CharField(max_length=200)
+    width = models.FloatField(0)
+    height = models.FloatField(0)
+    depth = models.FloatField(0)
+    artist_id = models.ForeignKey(Artists)
