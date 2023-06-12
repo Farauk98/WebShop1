2aa4c6a07e3d30c83ac94c8474b914e503579604
Farauk98 <kornat.rafal@gmail.com>
2023-06-12 20:34:34 +0200

    add app in settings; register models artists,paintings; add class view for Artists

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "webskop.apps.WebshopConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mysite/webshop/views.py
+++ mysite/webshop/views.py
@@ -1,5 +1,28 @@
+import requests
 from django.http import HttpResponse
+from django.views import View
+from .models import Artists
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the webshop index.")
+
+class ArtistsView(View):
+    def get(self, request):
+        api_url = "https://api.artic.edu/api/v1/artists"
+
+        response = requests.get(api_url)
+        data = response.json()
+
+        for artist_data in data['artists']:
+            title = artist_data['title']
+            birth_date = artist_data['birth_date']
+            death_date = artist_data['death_date']
+
+            artist, created = Artists.objects.get_or_create(title=title)
+
+            artist.birth_date = birth_date
+            artist.death_date = death_date
+            artist.save()
+
+        return HttpResponse("Obiekty Artists zostały zaktualizowane.")
