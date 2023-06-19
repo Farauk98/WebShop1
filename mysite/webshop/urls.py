87c26923be7c7f103ec6e525931aaf9ad8e15782
Farauk98 <kornat.rafal@gmail.com>
2023-06-19 23:52:02 +0200

    update urls for forms

--- mysite/webshop/urls.py
+++ mysite/webshop/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path('paintings/', views.PaintingsView.as_view(), name='paintings'),
     path('artists/', views.ArtistsView.as_view(), name='artists'),
+    path('paintings/create/', views.PaintingsCreateView.as_view(), name='create-paintings'),
+    path('artists/create/', views.ArtistsCreateView.as_view(), name='create-artists'),
 ]
